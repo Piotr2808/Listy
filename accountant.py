69b9af10212b8407c0cdcd3b4e062b02ecc370c1
Piotr <piotr_jasnowski@o2.pl>
2021-06-06 20:58:53 +0200

    koniec zadania

--- accountant.py
+++ accountant.py
@@ -1,25 +1,7 @@
-# TUTAJ TOWRZE PLIKI DO WCZYTYWANIA I ZAPISU
-
-magazyn = open("magazyn.txt", "w")
-magazyn_read = open("magazyn.txt", "r")
 saldo = open("saldo.txt", "a+")
 saldo_read = open("saldo.txt", "r+")
-historia = open("historia.txt", "w")
-historia_read = open("historia.txt", "r")
 
 import linecache
-# TUTAJ KOMBINUJE Z WCZYTANIEM STR DO DICT ... I COŚ MI SIĘ MIESZA. CHCE TO ZROBIĆ TAK ABY CO DRUGI ELEMENT ZE ZMIENNEJ
-# ,,RZECZ'' PRZYPISYWAŁO MI DO DWÓCH INNY ZMIENNYCH I ZAPISYWAŁO DO SŁOWNIKA. PIERWSZA TO STR OD ' DO ' A DRUGA TO
-# WARTOŚC OD : DO , NA TO WYCHOPDZI. NIE WIEM CZY TAK SIĘ DA. CAŁEGO ZADANIA NIE PRZEROBIE NA TE KLASY BO
-# BO JESZCZE SIĘ W TYM GUBIEM, A  PO PRACY NIE WYSTARCZA MI CZASU ...
-
-for line in magazyn_read.readline().split('{'):
-    rzecz = line.split('}')[0]
-    for h in rzecz[::2]:
-        e = rzecz.strip("''").lstrip("'")
-
-
-# TUTAJ WCZYTUJE SALDO
 
 saldo_line = len(open("saldo.txt", "r").readlines())
 wiersz_saldo = linecache.getline("saldo.txt", saldo_line)
@@ -32,33 +14,27 @@ for s in wiersz_saldo.split():
 
 ALLOWED_COMMAND = ["account balance", "sale", "purchase", "account", "stop"]
 
-warehouse = {"mountain bike": 3,
-             "sport shoes": 5,
-             "ball": 4}
+warehouse = {}
 choose_items = {}
 amount_of_money = aom
 history_sale = {}
 history_purchase = {}
 history = []
-to_save = warehouse
-
+def his_read():
+    with open("historia.txt", "r") as file_:
+        for l_ in file_.readlines():
+            line_ = l_.split("\n")[0]
+            history.append(line_)
+def ware_read():
+    with open("magazyn.txt", "r") as file:
+        for l in file.readlines():
+            name = l.split("\n")[0].split(",")[0]
+            wart = int(l.split("\n")[0].split(",")[1])
+            warehouse[name] = wart
+ware_read()
+his_read()
 
 while True:
-
-    # PROBLEM MAM TUTAJ. SAM ZAPIS CHYBA OK. NIE POTRAFIE WCZYTAĆ MAGAZYNU DO SŁOWNIKA. SALDO WCZYTUJE MI
-    # O ILE JEST JAKAŚ WARTOŚĆ POCZĄTKOWA JUŻ W .TXT. NP. 1000.
-    # Z HISTORIĄ JESZCZE NIE PRÓBOWAŁEM NIC ROBIĆ POZA SAMYM ZAPISEM.
-    # TO WERSJA KTÓREJ NIE PRZEROBIŁEM NA KLASY. NA KLASY PRZERABIAM SOBIE TO W INNYM PLIKU ALE TO MI JUŻ CAŁKIEM
-    # NIE IDZIE. WIĘC TAK. SALDO ZAPISUJE I WCZYTUJE. MAGAZYN ZAPISUJE ALE PROBLEM MAM Z WCZYTANIEM GO DO SŁOWNIKA
-    # A HISTORII JESZCZE NIE RUSZAŁEM. ONA JEST ZAPISYWANA W 3 MIEJSCACH. HISORIA SPRZEDAŻY, ZAKUPÓW I OGÓLNA.
-    #
-
-    magazyn.write(str(f"{warehouse}\n"))
-    saldo.write(f"{amount_of_money}\n")
-    historia.write(f"Historia: {history}\n"
-                       f"Historia zakupów: {history_purchase}\n"
-                       f"Historia sprzedaży: {history_sale}\n")
-
     print("\nKomendy: account balance, sale, purchase, account, stop.")
     print("\nUwaga! Komenda: stop wyłącza program!")
     comm = input("Komenda: ")
@@ -81,7 +57,7 @@ while True:
                     price = float(input("Cena: ")) * numbs_elements
                     history_sale.update({element_sale: numbs_elements})
                     amount_of_money += price
-                    summary_sale = [(f"Sprzedano {numbs_elements}: {element_sale} w cenie: {price} zł")]
+                    summary_sale = [(f"Sprzedano {numbs_elements}: {element_sale} w cenie: {price} zl")]
                     history += summary_sale
                     print(summary_sale)
                 if not warehouse[element_sale] > 0:
@@ -103,20 +79,20 @@ while True:
                 price = float(input("Cena: ")) * numbs_elements
                 amount_of_money -= price
                 if amount_of_money > 0:
-                    summary_purchase = [(f"Zakupiono {numbs_elements}: {choose_items_to_purchase} w cenie: {price} zł")]
+                    summary_purchase = [(f"Zakupiono {numbs_elements}: {choose_items_to_purchase} w cenie: {price} zl")]
                     print(summary_purchase)
                     history += summary_purchase
                 if amount_of_money < 0:
                     print("Bankrut")
                     break
             if not choose_items_to_purchase in warehouse:
-                numbs_elements = int(input("Ilość ryju: "))
+                numbs_elements = int(input("Ilość: "))
                 history_purchase.update({choose_items_to_purchase: numbs_elements})
                 warehouse[choose_items_to_purchase] = numbs_elements
                 price_items = float(input("Cena: "))
                 price = price_items * numbs_elements
                 amount_of_money -= price
-                summary_purchase = [(f"Zakupiono {numbs_elements}: {choose_items_to_purchase} w cenie: {price} zł")]
+                summary_purchase = [(f"Zakupiono {numbs_elements}: {choose_items_to_purchase} w cenie: {price} zl")]
                 if amount_of_money > 0:
                     history += summary_purchase
                 if amount_of_money < 0:
@@ -155,4 +131,11 @@ while True:
         print("BAD COMMAND")
         continue
     if comm == ALLOWED_COMMAND[4]:
+        magazyn = open("magazyn.txt", "w")
+        for name, value in warehouse.items():
+            magazyn.write(str(name) + ',' + str(value) + '\n')
+        saldo.write(f"\n{amount_of_money}\n")
+        his = open("historia.txt", "w")
+        for name_ in history:
+            his.write(str(name_) + '\n')
         break
